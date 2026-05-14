f6568ce055a9678677bd402ae9e4e1f7bdea1ed0
MEDICBACLIEU\ducpm <ducpm@medicbaclieu.vn>
2026-05-14 11:01:32 +0700

    update: icon menu sidebar ví của tôi

--- src/_mock/_data.ts
+++ src/_mock/_data.ts
@@ -100,24 +100,14 @@ export const _products = [...Array(24)].map((_, index) => {
 
 export const _langs = [
   {
-    value: 'en',
-    label: 'English',
-    icon: '/assets/icons/flags/ic-flag-en.svg',
-  },
-  {
     value: 'vi',
     label: 'Tiếng Việt',
     icon: '/assets/icons/flags/flag-for-flag-vietnam.svg',
   },
   {
-    value: 'de',
-    label: 'German',
-    icon: '/assets/icons/flags/ic-flag-de.svg',
-  },
-  {
-    value: 'fr',
-    label: 'French',
-    icon: '/assets/icons/flags/ic-flag-fr.svg',
+    value: 'en',
+    label: 'English',
+    icon: '/assets/icons/flags/ic-flag-en.svg',
   },
 ];
 
